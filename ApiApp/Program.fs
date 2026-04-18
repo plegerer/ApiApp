5abd44ab4d3122f7abce5afe5c97b14e7be0d8f1
plegerer <56559282+plegerer@users.noreply.github.com>
2026-04-18 13:26:47 +0000

    Add message bus connection

--- ApiApp/Program.fs
+++ ApiApp/Program.fs
@@ -1,11 +1,39 @@
+open System
+open System.IO
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
 open Falco
 open Falco.Routing
-open Microsoft.AspNetCore.Builder
+open Azure.Messaging.ServiceBus
 
-let wapp = WebApplication.Create()
+// POST Handler
+let postHandler : HttpHandler =
+    fun ctx ->
+        task {
+            use reader = new StreamReader(ctx.Request.Body)
+            let! body = reader.ReadToEndAsync()
+
+            let sender = ctx.RequestServices.GetService<ServiceBusSender>()
+
+            let message = ServiceBusMessage(body)
+            let! _ =  sender.SendMessageAsync(message)
+
+            return! Response.ofPlainText "Message sent" ctx
+        }
+
+let builder = WebApplication.CreateBuilder()
+let connectionString = Environment.GetEnvironmentVariable("SERVICEBUS_CONNECTION_STRING")
+let queueName = Environment.GetEnvironmentVariable("SERVICEBUS_QUEUE_NAME")
+let client = new ServiceBusClient(connectionString)
+let sender = client.CreateSender(queueName)
+
+builder.Services.AddSingleton<ServiceBusSender>(sender) |> ignore
+
+let wapp = builder.Build()
 
 wapp.UseRouting()
     .UseFalco([
         get "/" (Response.ofPlainText "Hello World!")
+        post "/api/messages" postHandler
     ])
     .Run(Response.ofPlainText "Not found")
